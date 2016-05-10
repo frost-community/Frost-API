0c36e4db32f5f803098dc367db3dbc3ed473168c
Marihachi <marihachi0620@gmail.com>
2016-05-11 02:14:49 +0900

    fix indent

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ require_once __DIR__.'/config.php';
 require_once __DIR__.'/util/utils.php';
 
 $appConfig = [
-    'settings' => [
-        'displayErrorDetails' => true
-    ],
-    'config' => $config,
+	'settings' => [
+		'displayErrorDetails' => true
+	],
+	'config' => $config,
 	'dbManager' => new DatabaseManager($config['db']['hostname'], $config['db']['username'], $config['db']['password'], $config['db']['dbname'])
 ];
 
