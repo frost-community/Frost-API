39e89a89f8c8fead2782ba2b06571fa27e6d5204
Marihachi <marihachi0620@gmail.com>
2016-05-11 04:17:33 +0900

    update

--- controllers/web.php
+++ controllers/web.php
@@ -2,11 +2,25 @@
 
 class Web
 {
-	public static function requestId($params, $res, $container)
+	public static function createRequestKey($params, $res, $container)
 	{
 		$requireParams = ['user-key'];
 
 		if (!hasRequireParams($params, $requireParams))
 			return withFailure($res, 'required parameters are missing', $requireParams);
+
+		if (!UserKey::validate($params['user-key'], $container->dbManager))
+			return withFailure($res, 'parameters are invalid', ['user-key']);
+
+		try
+		{
+			$requestKey = RequestKey::create($params['user-key'], $container->config, $container->dbManager);
+		}
+		catch(Exception $e)
+		{
+			return withFailure($res, 'failed to create request key', ['detail' => $e->getMessage()]);
+		}
+
+		return withSuccess($res, 'successful', ['request-key'=>$requestKey]);
 	}
 }
