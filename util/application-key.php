9f43e6a8c591298d2313b4891ed6dbba0cf614d4
Marihachi <marihachi0620@gmail.com>
2016-05-11 05:34:40 +0900

    update

--- util/application-key.php
+++ util/application-key.php
@@ -3,13 +3,38 @@ namespace Utility;
 
 class ApplicationKey
 {
-	public static function create()
+	public static function create($userId, $name, $description, $config, DatabaseManager $db)
 	{
-		
+		$now = time();
+
+		// TODO: 既存のアプリケーションに同名のものがあれば例外
+
+		try
+		{
+			$container->dbManager->executeQuery('insert into frost_application (creator_id, created_at, name, description) values(?, ?, ?, ?)', [$userId, $now, $name, $description]);
+		}
+		catch(PDOException $e)
+		{
+			throw new Exception('faild to create database record');
+		}
+
+		$application = $container->dbManager->executeQuery('select * from frost_application where creator_id = ? & name = ?', [$userId, $name])->fetch()[0];
+
+		return $application;
 	}
 
-	public static function validate()
+	public static function validate($config, DatabaseManager $db)
 	{
-		
+		//TODO
+	}
+	
+	public static function generateKey($config, DatabaseManager $db)
+	{
+		//TODO
+	}
+
+	public static function validateKey()
+	{
+		//TODO
 	}
 }
