686cdc011187b663023d388eb7d417b13c5acfd5
Marihachi <marihachi0620@gmail.com>
2016-05-11 03:30:26 +0900

    update request key functions

--- util/request-key.php
+++ util/request-key.php
@@ -1,39 +1,46 @@
 <?php
 
-require_once(__FILE__.'/regex.php');
+// RequestKey はFrost-Webからのアクセスのみを許可するAPIでそのリクエストが正当であるかどうかを検証するためのキーです。
+// ここではそのキーを生成・検証・削除するための関数を提供します。
 
 function createRequestKey($userId, $config, DatabaseManager $db, $requestId = null, $num = null)
 {
-	if ($requestId === null)
+	$requestReceptions = $db->executeQuery('select * from frost_request_reception where user_id = ?', [$userId])->fetch();
+
+	// すでにキーが発行済みであれば
+	if (count($requestReceptions) !== 0)
 	{
-		do
+		$requestReception = $requestReceptions[0];
+
+		$requestId = $requestReception['id'];
+		$num = $requestReception['salt'];
+	}
+	else
+	{
+		if ($requestId === null)
 		{
-			$requestId = rand(1, 999999);
-			$requestReceptions = $db->executeQuery('select * from frost_request_reception where id = ?', [$requestId])->fetch();
-			$tryCount++;
-			$isUsablerequestId = count($requestReceptions) === 0;
+			do
+			{
+				$requestId = rand(1, 999999);
+				$requestReceptions = $db->executeQuery('select * from frost_request_reception where id = ?', [$requestId])->fetch();
+				$tryCount++;
+				$isUsablerequestId = count($requestReceptions) === 0;
+			}
+			while(!$isUsablerequestId && $tryCount < 1000);
+
+			if(!$isUsablerequestId)
+				throw new Exception('faild to create request-key');
 		}
-		while(!$isUsablerequestId && $tryCount < 1000);
-		
-		if(!$isUsablerequestId)
-			return null;
+
+		if ($num === null)
+			$num = rand(1, 99999);
+
+		$now = time();
+
+		$db->executeQuery('insert into frost_request_reception (created_at, id, user_id, salt) values (?, ?, ?, ?)', [$now, $requestId, $userId, $num]);
 	}
 
-	if ($num === null)
-		$num = rand(1, 99999);
-
-	$now = time();
-
-	try
-	{
-		$db->executeQuery('insert into frost_request_reception (created_at, request_id, user_id) values (?, ?, ?)', [$now, $requestId, $userId]);
-	}
-	catch(PDOException $e)
-	{
-		return null;
-	}
-
-	return $userId.'-'.$requestId.'-'.$num.'-'.hash('sha256', $config['keyBase'].$userId.$requestId.$num);
+	return [$userId.'-'.$requestId.'-'.$num.'-'.hash('sha256', $config['keyBase'].$userId.$requestId.$num), $requestId];
 }
 
 function validateRequestKey($requestKey, $config, DatabaseManager $db)
@@ -50,4 +57,11 @@ function validateRequestKey($requestKey, $config, DatabaseManager $db)
 	$correctRequestKey = $userId.'-'.$requestId.'-'.$num.'-'.hash('sha256', $config['keyBase'].$userId.$requestId.$num);
 
 	return $requestKey === $correctRequestKey;
+}
+
+function destroyRequestKey(DatabaseManager $db, $requestId = null)
+{
+	$db->executeQuery('delete from frost_request_reception where id = ?', [$requestId]);
+
+	return true;
 }
