f8c3960b9fc6d891621fe8dd1813dfa899788b98
Marihachi <marihachi0620@gmail.com>
2016-05-13 12:38:36 +0900

    Update account.php

--- controllers/account.php
+++ controllers/account.php
@@ -6,7 +6,7 @@ class Account
 	{
 		$params = $req->getParams();
 
-		$requireParams = ['screen_name', 'password'];
+		$requireParams = ['screen-name', 'password'];
 
 		if (!hasRequireParams($params, $requireParams))
 			return withFailure($res, 'required parameters are missing', $requireParams);
@@ -31,28 +31,28 @@ class Account
 		$isOccurredError = false;
 		$errorTargets = [];
 
-		if (!Regex::isMatch('/^[a-z0-9_]{4,15}$/i', $params['screen_name']) || Regex::isMatch('/(.)\1{3,}/', $params['screen_name']))
+		if (!Regex::isMatch('/^[a-z0-9_]{4,15}$/i', $params['screen-name']) || Regex::isMatch('/(.)\1{3,}/', $params['screen-name']))
 		{
 			$isOccurredError = true;
-			$errorTargets[] = 'screen_name';
+			$errorTargets[] = 'screen-name';
 		}
 		else
 		{
-			$isExistUser = count($container->dbManager->executeQuery('select * from frost_account where screen_name = ? limit 1', [$params['screen_name']])->fetch()) === 1;
+			$isExistUser = count($container->dbManager->executeQuery('select * from frost_account where screen_name = ? limit 1', [$params['screen-name']])->fetch()) === 1;
 
 			if ($isExistUser)
 			{
 				$isOccurredError = true;
-				$errorTargets[] = 'screen_name';
+				$errorTargets[] = 'screen-name';
 			}
 			else
 			{
 				foreach ($invalidSN as $i)
 				{
-					if ($params['screen_name'] === $i)
+					if ($params['screen-name'] === $i)
 					{
 						$isOccurredError = true;
-						$errorTargets[] = 'screen_name';
+						$errorTargets[] = 'screen-name';
 					}
 				}
 			}
@@ -69,9 +69,9 @@ class Account
 
 		$createdAt = time();
 		$passwordHash = hash('sha256', $params['password'].$createdAt);
-		$container->dbManager->executeQuery('insert into frost_account (created_at, screen_name, name, password_hash) values(?, ?, ?, ?)', [$createdAt, $params['screen_name'], "froster", $passwordHash]);
+		$container->dbManager->executeQuery('insert into frost_account (created_at, screen_name, name, password_hash) values(?, ?, ?, ?)', [$createdAt, $params['screen-name'], "froster", $passwordHash]);
 
-		$user = $container->dbManager->executeQuery('select * from frost_account where screen_name = ? limit 1', [$params['screen_name']])->fetch();
+		$user = $container->dbManager->executeQuery('select * from frost_account where screen_name = ? limit 1', [$params['screen-name']])->fetch();
 
 		return withSuccess($res);
 	}
