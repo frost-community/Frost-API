b82ea2c7f8cee1c38a59ee983463a38bf3194dec
Marihachi <marihachi0620@gmail.com>
2016-05-18 04:30:36 +0900

    update account/create

--- controllers/account.php
+++ controllers/account.php
@@ -11,68 +11,15 @@ class Account
 		if (!hasRequireParams($params, $requireParams))
 			return withFailure($res, 'required parameters are missing', $requireParams);
 
-		$invalidSN = [
-			'frost',
-			'help',
-			'home',
-			'mentions',
-			'login',
-			'logout',
-			'search',
-			'signin',
-			'signup',
-			'signout',
-			'welcome',
-			'static',
-			'application',
-			'developer',
-		];
-
-		$isOccurredError = false;
-		$errorTargets = [];
-
-		if (!Regex::isMatch('/^[a-z0-9_]{4,15}$/i', $params['screen-name']) || Regex::isMatch('/(.)\1{3,}/', $params['screen-name']))
+		try
 		{
-			$isOccurredError = true;
-			$errorTargets[] = 'screen-name';
+			$account = User::create($params['screen-name'], $params['password'], 'froster');
 		}
-		else
+		catch(ApiException $e)
 		{
-			$isExistUser = count($container->dbManager->executeQuery('select * from frost_account where screen_name = ? limit 1', [$params['screen-name']])->fetch()) === 1;
-
-			if ($isExistUser)
-			{
-				$isOccurredError = true;
-				$errorTargets[] = 'screen-name';
-			}
-			else
-			{
-				foreach ($invalidSN as $i)
-				{
-					if ($params['screen-name'] === $i)
-					{
-						$isOccurredError = true;
-						$errorTargets[] = 'screen-name';
-					}
-				}
-			}
+			return withFailure($res, $e->getMessage(), $e->getData());
 		}
 
-		if (!Regex::isMatch('/^[a-z0-9_-]{6,128}$/i', $params['password']))
-		{
-			$isOccurredError = true;
-			$errorTargets[] = 'password';
-		}
-
-		if ($isOccurredError)
-			return withFailure($res, 'parameters are invalid', $errorTargets);
-
-		$createdAt = time();
-		$passwordHash = hash('sha256', $params['password'].$createdAt);
-		$container->dbManager->executeQuery('insert into frost_account (created_at, screen_name, name, password_hash) values(?, ?, ?, ?)', [$createdAt, $params['screen-name'], "froster", $passwordHash]);
-
-		$user = $container->dbManager->executeQuery('select * from frost_account where screen_name = ? limit 1', [$params['screen-name']])->fetch();
-
-		return withSuccess($res);
+		return withSuccess('successful', ['account'=>$account]);
 	}
 }
