aeca92491e1ac535119bac1c58efb7885aa1f115
Marihachi <marihachi0620@gmail.com>
2016-05-18 05:56:39 +0900

    add config sample

---
+++ config_sample.php
@@ -0,0 +1,14 @@
+<?php
+
+$config = [
+	'access-key-base' => 'keybase1',
+	'application-key-base' => 'keybase2',
+	'request-key-base' => 'keybase3',
+	'user-key-base' => 'keybase4',
+	'db' => [
+		'hostname' => 'your-database-hostname',
+		'dbname' => 'your-database-name',
+		'username' => 'your-database-username',
+		'password' => 'your-database-password',
+	]
+];
