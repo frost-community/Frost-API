d8d7c076909d9dbb8a1a9689e14b09a9e2a9448e
Marihachi <marihachi0620@gmail.com>
2016-05-31 19:32:40 +0900

    nya

--- router.php
+++ router.php
@@ -13,18 +13,18 @@ function indexPage ($req, $res, $container)
 }
 
 $routes = [
-	['method'=>'get',  'endpoint'=>'/',                                     'permissions'=>[], 'indexPage'],
-	['method'=>'get',  'endpoint'=>'/ice-auth/request-key',                 'permissions'=>[], 'indexPage'],
-	['method'=>'get',  'endpoint'=>'/ice-auth/authorize',                   'permissions'=>[], 'indexPage'],
-	['method'=>'post', 'endpoint'=>'/ice-auth/authorize',                   'permissions'=>[], 'indexPage'],
-	['method'=>'get',  'endpoint'=>'/ice-auth/access-key',                  'permissions'=>[], 'indexPage'],
-	['method'=>'post', 'endpoint'=>'/internal/account/create',              'permissions'=>['internal'], 'indexPage'],
-	['method'=>'post', 'endpoint'=>'/internal/application/create',          'permissions'=>['internal'], 'Application::create'],
-	['method'=>'post', 'endpoint'=>'/internal/application/regenerate-key',  'permissions'=>['internal'], 'indexPage'],
-	['method'=>'get',  'endpoint'=>'/internal/application/application-key', 'permissions'=>['internal'], 'indexPage'],
-	['method'=>'get',  'endpoint'=>'/user',                                 'permissions'=>['user-read'], 'indexPage'],
-	['method'=>'get',  'endpoint'=>'/post/timeline',                        'permissions'=>['post-read'], 'Post::timeline'],
-	['method'=>'post', 'endpoint'=>'/post/create',                          'permissions'=>['post-write'], 'Post::create']
+	['method'=>'get',  'endpoint'=>'/',                            'permissions'=>[], 'indexPage'],
+	['method'=>'get',  'endpoint'=>'/ice-auth/request-key',        'permissions'=>[], 'ice-auth::requestKey'],
+	['method'=>'get',  'endpoint'=>'/ice-auth/authorize',          'permissions'=>[], 'ice-auth::authorizePage'],
+	['method'=>'post', 'endpoint'=>'/ice-auth/authorize',          'permissions'=>[], 'ice-auth::authorize'],
+	['method'=>'get',  'endpoint'=>'/ice-auth/access-key',         'permissions'=>['internal'], 'ice-auth::accessKey'],
+	['method'=>'post', 'endpoint'=>'/account/create',              'permissions'=>['internal'], 'indexPage'],
+	['method'=>'post', 'endpoint'=>'/application/create',          'permissions'=>['internal'], 'Application::create'],
+	['method'=>'post', 'endpoint'=>'/application/regenerate-key',  'permissions'=>['internal'], 'indexPage'],
+	['method'=>'get',  'endpoint'=>'/application/application-key', 'permissions'=>['internal'], 'indexPage'],
+	['method'=>'get',  'endpoint'=>'/user',                        'permissions'=>['user-read'], 'indexPage'],
+	['method'=>'get',  'endpoint'=>'/post/timeline',               'permissions'=>['post-read'], 'Post::timeline'],
+	['method'=>'post', 'endpoint'=>'/post/create',                 'permissions'=>['post-write'], 'Post::create']
 ];
 
 foreach ($routes as $route)
@@ -32,11 +32,12 @@ foreach ($routes as $route)
 	$method = $route['method'];
 	$endPoint = $route['endpoint'];
 
-	$app->$method($endPoint, function ($req, $res, $args) use($routes, $route, $endPoint)
+	$app->$method($endPoint, function ($req, $res, $args) use($route, $endPoint)
 	{
 		// TODO: validate access-key
 
 		// TODO: validate permissions
+		$requirePermissions = $route['permissions'];
 
 		if(!is_callable(current(array_slice($route, -1, 1))))
 			throw new Exception("last item of route was non-callable (endpoint: $endPoint)");
