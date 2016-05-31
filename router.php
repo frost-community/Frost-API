0cac2792f5294ede6c6d6ae9727b7577291d9f28
Marihachi <marihachi0620@gmail.com>
2016-05-31 23:15:21 +0900

    update

--- router.php
+++ router.php
@@ -27,34 +27,44 @@ $routes = [
 	['method'=>'post', 'endpoint'=>'/post/create',                 'permissions'=>['post-write'], 'Post::create']
 ];
 
-foreach ($routes as $route) {
+foreach ($routes as $route)
+{
 	$method = $route['method'];
 	$endPoint = $route['endpoint'];
 
-	$app->$method($endPoint, function ($req, $res, $args) use($route, $endPoint) {
-		$params = $req->getParams();
-		if (!array_key_exists('access-key', $params))
-			return withFailure($res, 'access-key is missing');
+	$app->$method($endPoint, function ($req, $res, $args) use($route, $endPoint)
+	{
+		if (count($route['permissions']) !== 0)
+		{
+			$params = $req->getParams();
+			if (!array_key_exists('access-key', $params))
+				return withFailure($res, 'access-key is missing');
 
-		$applicationAccess = \Models\ApplicationAccess::validate($params['access-key'], $this);
-		if (!$applicationAccess)
-			return withFailure($res, 'access-key is invalid');
+			$applicationAccess = \Models\ApplicationAccess::validate($params['access-key'], $this);
+			if (!$applicationAccess)
+				return withFailure($res, 'access-key is invalid');
 
-		$user = \Models\User::fetch($applicationAccess['user_id'], $this);
-		$application = \Models\Application::fetch($applicationAccess['application_id'], $this);
+			$user = \Models\User::fetch($applicationAccess['user_id'], $this);
+			$application = \Models\Application::fetch($applicationAccess['application_id'], $this);
 
-		$permissions = explode(',', $application['permissions']);
+			$permissions = explode(',', $application['permissions']);
 
-		foreach ($route['permissions'] as $requirePermission) {
-			if (array_key_exists($requirePermission, $permissions))
-				return withFailure($res, 'You do not have some permissions.');
+			foreach ($route['permissions'] as $requirePermission)
+			{
+				if (array_key_exists($requirePermission, $permissions))
+					return withFailure($res, 'You do not have some permissions.');
+			}
+
+			$controllerArgs = [$req, $res, $this, $user, $application];
+		}
+		else
+		{
+			$controllerArgs = [$req, $res, $this];
 		}
 
 		if(!is_callable(current(array_slice($route, -1, 1))))
 			throw new Exception("last item of route was non-callable (endpoint: $endPoint)");
 		$callable = current(array_slice($route, -1, 1));
-
-		$controllerArgs = [$req, $res, $this, $user, $application];
 
 		return call_user_func_array($callable, $controllerArgs);
 	});
