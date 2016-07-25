12ed56272ce6f4d1cc234330dbe683004a384008
Marihachi <marihachi0620@gmail.com>
2016-07-25 11:05:45 +0900

    split from router to routes

--- router.php
+++ router.php
@@ -1,26 +1,6 @@
 <?php
 
-function indexPage ($req, $res, $container)
-{
-	$res->getBody()->write('Frost API Server');
-	return $res;
-}
-
-$routes = [
-	['method'=>'get',  'endpoint'=>'/',                            'permissions'=>[], 			 'indexPage'],
-	['method'=>'get',  'endpoint'=>'/ice-auth/request-key',        'permissions'=>[], 			 'IceAuth::requestKey'],
-	['method'=>'get',  'endpoint'=>'/ice-auth/authorize',          'permissions'=>[], 			 'IceAuth::authorizePage'],
-	['method'=>'get',  'endpoint'=>'/ice-auth/access-key',         'permissions'=>['internal'],  'IceAuth::accessKey'],
-	['method'=>'get',  'endpoint'=>'/application/application-key', 'permissions'=>['internal'],  'Application::applicationKey'],
-	['method'=>'get',  'endpoint'=>'/user',                        'permissions'=>['user-read'], 'User::show'],
-	['method'=>'get',  'endpoint'=>'/user/timeline',               'permissions'=>['user-read'], 'User::timeline'],
-	['method'=>'get',  'endpoint'=>'/post/timeline',               'permissions'=>['post-read'], 'Post::timeline'],
-	['method'=>'post', 'endpoint'=>'/post/create',                 'permissions'=>['post-write'], 'Post::create'],
-	['method'=>'post', 'endpoint'=>'/application/regenerate-key',  'permissions'=>['internal'],  'Application::regenerateKey'],
-	['method'=>'post', 'endpoint'=>'/account/create',              'permissions'=>['internal'],  'User::create'],
-	['method'=>'post', 'endpoint'=>'/application/create',          'permissions'=>['internal'],  'Application::create'],
-	['method'=>'post', 'endpoint'=>'/ice-auth/authorize',          'permissions'=>[], 			 'IceAuth::authorize']
-];
+require_once(__DIR__.'routes.php');
 
 foreach ($routes as $route)
 {
