e2373a00032e7f0da0ccad7443aab307c685a65e
Marihachi <marihachi0620@gmail.com>
2016-07-25 11:15:13 +0900

    Change config from sample file to actually file.

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__.'/vendor/autoload.php';
-require_once __DIR__.'/config_sample.php';
+require_once __DIR__.'/config.php';
 
 require_once __DIR__.'/util/utils-loader.php';
 require_once __DIR__.'/models/models-loader.php';
