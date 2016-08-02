981e1282ecebe819263d0995a666d4f3e22ba343
Marihachi <marihachi0620@gmail.com>
2016-08-03 04:36:12 +0900

    update table information

--- create_database_tables.sql
+++ create_database_tables.sql
@@ -7,8 +7,7 @@ CREATE TABLE IF NOT EXISTS `frost_application` (
   `name` VARCHAR(256) NOT NULL,
   `description` VARCHAR(256) NOT NULL,
   `permissions` VARCHAR(256) NOT NULL,
-  `key_hash` VARCHAR(256) NULL,
-  `management_code` INT(5) NULL,
+  `key_code` INT(5) NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `name_UNIQUE` (`name`))
 ENGINE=MyISAM DEFAULT CHARSET=utf8;
@@ -18,8 +17,7 @@ CREATE TABLE IF NOT EXISTS `frost_application_access` (
   `created_at` INT NOT NULL,
   `user_id` INT NOT NULL,
   `application_id` INT NOT NULL,
-  `key_hash` VARCHAR(256) NULL,
-  `management_code` INT(5) NULL,
+  `key_code` INT(5) NULL,
   PRIMARY KEY (`id`))
 ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -27,8 +25,7 @@ CREATE TABLE IF NOT EXISTS `frost_request` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `created_at` INT NOT NULL,
   `application_id` INT NOT NULL,
-  `key_hash` VARCHAR(256) NULL,
-  `management_code` INT(5) NULL,
+  `key_code` INT(5) NULL,
   PRIMARY KEY (`id`))
 ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
