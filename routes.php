562cb86b17fc8521014d37e06946be5ee32fc1ef
Marihachi <marihachi0620@gmail.com>
2016-08-06 18:15:13 +0900

    update routes

--- routes.php
+++ routes.php
@@ -5,6 +5,10 @@ function indexPage ($req, $res, $container)
 	return withSuccess($res, null, 'Frost API Server');
 }
 
+// post:   作成・更新
+// get:    取得
+// delete: 削除
+
 $routes = [
 	// Top
 	['get',    '/',                            [],                      'indexPage'],
@@ -18,19 +22,22 @@ $routes = [
 	['post',   '/application',                 ['application-special'], 'ApplicationController::create'],                 // アプリ情報生成
 	['get',    '/application',                 ['application'],         'ApplicationController::show'],                   // アプリ情報表示
 	['post',   '/application/application-key', ['application-special'], 'ApplicationController::applicationKeyGenerate'], // アプリキー生成
-	['get',    '/application/application-key', ['application-special'], 'ApplicationController::applicationKey'],         // アプリキー表示
+	['get',    '/application/application-key', ['application-special'], 'ApplicationController::applicationKeyShow'],     // アプリキー表示
 
 	// User
 	['get',    '/user',                        ['user-read'],           'UserController::show'],
 	['get',    '/user/timeline',               ['user-read'],           'UserController::timeline'],
-	['post',   '/user/follow',                 ['user-write'],          'UserController::follow'],
-	['delete', '/user/follow',                 ['user-write'],          'UserController::unfollow'],
+	['get',    '/user/followings',             ['user-read'],           'UserController::followings'],
+	['get',    '/user/followers',              ['user-read'],           'UserController::followers'],
+	['post',   '/user/follow',                 ['user-write'],          'UserController::followCreate'],
+	['delete', '/user/follow',                 ['user-write'],          'UserController::followDestroy'],
 
 	// Account
-	['post',   '/account',                     ['account-special'],     'UserController::create'],
+	['post',   '/account',                     ['account-special'],     'AccountController::create'],
 
 	// Post
-	['post',   '/post',                        ['post-write'],          'PostController::create'],
+	['post',   '/post/status',                 ['post-write'],          'PostController::statusCreate'],
+	['post',   '/post/article',                ['post-write'],          'PostController::articleCreate'],
 	['get',    '/post',                        ['post-read'],           'PostController::show'],
 	['get',    '/post/timeline',               ['post-read'],           'PostController::timeline']
 ];
