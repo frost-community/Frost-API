ac3ad4ca32845f157108c62ae3deaf8a7b55186a
Marihachi <marihachi0620@gmail.com>
2016-08-10 22:55:35 +0900

    [wip]improve architecture - user

---
+++ data-models/user.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * ユーザーのレコードに対する操作を提供します
+ */
+class UserData
+{
+	private $userFactory;
+	public $record;
+
+	public function __construct(UserFactory $userFactory, $record)
+	{
+		if ($userFactory === null || $record === null)
+			throw new \Exception('argument is empty');
+
+		$this->userFactory = $userFactory;
+		$this->record = $record;
+	}
+
+	/**
+	 * アプリケーションを取得します
+	 */
+	public function applications(ApplicationFactory $applicationFactory)
+	{
+		return $applicationFactory->findManyWithFilters(['user_id', $this->record->id]);
+	}
+
+	/**
+	 * アプリケーションアクセスを取得します
+	 */
+	public function applicationAccesses(ApplicationAccessFactory $applicationAccessFactory)
+	{
+		return $applicationAccessFactory->findManyWithFilters(['user_id', $this->record->id]);
+	}
+
+	/**
+	 * レスポンス向けの配列データに変換します
+	 *
+	 * @return array レスポンス向けの配列データ
+	 */
+	public function toArrayResponse()
+	{
+		$data = [
+			'id' => $this->record->id,
+			'created_at' => $this->record->created_at,
+			'screen_name' => $this->record->creator_id,
+			'name' => $this->record->name
+		];
+
+		return $data;
+	}
+}
---
+++ factories/user.php
@@ -0,0 +1,144 @@
+<?php
+
+/**
+ * ユーザーを作成、検索する手段を提供します
+ */
+class UserFactory
+{
+	private $database;
+	private $config;
+	private $regex;
+
+	public function __construct(DatabaseAccess $database, $config, \Utility\Regex $regex)
+	{
+		if ($database === null || $config === null || $regex === null)
+			throw new \Exception('argument is empty');
+
+		$this->database = $database;
+		$this->config = $config;
+		$this->regex = $regex;
+	}
+
+	/**
+	 * データベースのレコードを作成し、インスタンスを取得します
+	 */
+	public static function create($screenName, $password, $name)
+	{
+		if ($screenName === null || $password === null || $name === null)
+			throw new \Exception('some arguments are empty');
+
+		$timestamp = time();
+
+		$isOccurredError = false;
+		$errorTargets = [];
+
+		if (!$this->regex->isMatch('/^[a-z0-9_]{4,15}$/i', $screenName) || $this->regex->isMatch('/^(.)\1{3,}$/', $screenName))
+		{
+			$isOccurredError = true;
+			$errorTargets[] = 'screen-name';
+		}
+		else
+		{
+			if ($this->database->findOneWithFilters(['screen_name', $screenName]))
+			{
+				$isOccurredError = true;
+				$errorTargets[] = 'screen-name';
+			}
+			else
+			{
+				foreach ($container->config['invalid-screen-names'] as $invalidScreenName)
+				{
+					if ($screenName === $invalidScreenName)
+					{
+						$isOccurredError = true;
+						$errorTargets[] = 'screen-name';
+					}
+				}
+			}
+		}
+
+		if (!$this->regex->isMatch('/^[a-z0-9_-]{6,128}$/i', $password))
+		{
+			$isOccurredError = true;
+			$errorTargets[] = 'password';
+		}
+
+		if ($isOccurredError)
+			throw new \Utility\ApiException('parameters are invalid', $errorTargets);
+
+		$record = $this->database->create($this->config['db']['table-names']['user'], [
+			'created_at' => $timestamp,
+			'screen_name' => $screenName,
+			'name' => $name,
+			'password_hash' => hash('sha256', $password.$timestamp)
+		]);
+
+		return $record;
+	}
+
+	/**
+	 * 既存のレコードを取得してインスタンスを取得します
+	 *
+	 * @param int $applicationId アプリケーションID
+	 * @throws \Exception
+	 * @throws \Utility\ApiException
+	 * @return ApplicationData インスタンス
+	 */
+	public function find($requestId)
+	{
+		if ($requestId === null)
+			throw new \Exception('argument is empty');
+
+		$record = $this->database->find($this->config['db']['table-names']['request'], $requestId);
+
+		if (!$record)
+			throw new \Utility\ApiException('request not found', [], 404);
+
+		return new RequestData($this, $record);
+	}
+
+	/**
+	 * 条件によってレコードを検索してインスタンスを取得します
+	 *
+	 * @param array $wheres 条件の連想配列(where句)
+	 * @throws \Exception
+	 * @throws \Utility\ApiException
+	 * @return ApplicationData インスタンス
+	 */
+	public function findOneWithFilters(array $wheres)
+	{
+		if ($wheres === null)
+			throw new \Exception('argument is empty');
+
+		$record = $this->database->findOneWithFilters($this->config['db']['table-names']['request'], $wheres);
+
+		if (!$record)
+			throw new \Utility\ApiException('request not found', [], 404);
+
+		return new RequestData($this, $record);
+	}
+
+	/**
+	 * 条件によってレコードを検索して複数のインスタンスを取得します
+	 *
+	 * @param array $wheres 条件の連想配列(where句)
+	 * @throws \Exception
+	 * @throws \Utility\ApiException
+	 * @return array ApplicationDataの配列
+	 */
+	public function findManyWithFilters(array $wheres)
+	{
+		if ($wheres === null)
+			throw new \Exception('argument is empty');
+
+		$records = $this->database->findManyWithFilters($this->config['db']['table-names']['request'], $wheres);
+
+		if (count($records) === 0)
+			throw new \Utility\ApiException('request not found', [], 404);
+
+		foreach($records as $record)
+			array_push($instances, new RequestData($this, $record));
+
+		return $instances;
+	}
+}
