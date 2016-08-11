78e112a85c7217c141cb5f84b34fd088e65f641b
Marihachi <marihachi0620@gmail.com>
2016-08-12 03:16:34 +0900

    add

--- data-models/data-models-loader.php
+++ data-models/data-models-loader.php
@@ -4,3 +4,4 @@ require_once(__DIR__.'/application.php');
 require_once(__DIR__.'/application-access.php');
 require_once(__DIR__.'/request.php');
 require_once(__DIR__.'/user.php');
+require_once(__DIR__.'/user-following.php');
