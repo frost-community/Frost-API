60c14b9949ca165f2db801d83f37ea72a83980df
Marihachi <marihachi0620@gmail.com>
2016-08-16 03:17:43 +0900

    clean

--- models/models-loader.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once(__DIR__.'/account.php');
-require_once(__DIR__.'/application.php');
-require_once(__DIR__.'/ice-auth.php');
-// require_once(__DIR__.'/post.php');
-require_once(__DIR__.'/user.php');
