d46ccef2c3fa12cc118a1f0a59a5d4d1f1febf20
Marihachi <marihachi0620@gmail.com>
2016-08-17 18:06:52 +0900

    update routes

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,36 @@
 'use strict';
 
-const testAction = function *(req, res) {
-	console.log(`test`);
-}
-
 module.exports = () => {
 	var routes = [
-		// general
-		['get',  '/',                                 {permissions:[]}],
+		['get',  '/',                                 {}],
 
-		// applications
-		['post', '/applications',                     {permissions:[]}],
-		['get',  '/applications/:id',                 {permissions:[]}],
-		['post', '/applications/:id/application-key', {permissions:[]}],
-		['get',  '/applications/:id/application-key', {permissions:[]}],
+		// Account
+		['post', '/account',                          {permissions:['account-special']}],
 
-		// users
-		['get',  '/users/:id',                        {permissions:[]}],
+		// IceAuth
+		['post', '/ice-auth/request',                 {}] // 認証リクエスト(リクエストキー取得)
+		['get',  '/ice-auth/pin-code',                {permissions:['ice-auth-host']}], // リクエストキーからPINコード取得(認証ホスト専用)
+		['post', '/ice-auth/authorize',               {}],
 
-		// account
-		['post', '/account',                          {permissions:[]}],
+		// Applications
+		['post', '/applications',                     {permissions:['application-special']}],
+		['get',  '/applications/:id',                 {permissions:['application']}],
+		['post', '/applications/:id/application-key', {permissions:['application-special']}],
+		['get',  '/applications/:id/application-key', {permissions:['application-special']}],
+
+		// Users
+		['get',  '/users/:id',                        {permissions:['user-read']}],
+		['get',  '/users/:id/timeline',               {permissions:['user-read']}],
+		['get',  '/users/:id/followings',             {permissions:['user-read']}],
+		['get',  '/users/:id/followers',              {permissions:['user-read']}],
+		['post', '/users/:id/follow',                 {permissions:['user-write']}],
+		['del',  '/users/:id/follow',                 {permissions:['user-write']}],
+
+		// Posts
+		['post', '/posts/status',                      {permissions:['post-write']}], // ステータスポストを作成する
+		['post', '/posts/article',                     {permissions:['post-write']}], // 記事を作成する
+		['get',  '/posts/timeline',                    {permissions:['post-read']}],
+		['get',  '/posts/{id}',                        {permissions:['post-read']}],
 	];
 
 	return routes;
