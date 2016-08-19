b1dc28b8172417e4f3f0ca940f28929041a33a22
Marihachi <marihachi0620@gmail.com>
2016-08-19 19:12:57 +0900

    update

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const co = require('co');
-const router = require('./module/router');
 const routes = require('./routes');
 const loadConfig = require('./module/load-config');
 
@@ -12,27 +11,32 @@ module.exports = () => {
 	console.log('  Frost API Server  ');
 	console.log('--------------------');
 
-	const app = express();
 	const config = loadConfig();
+	const app = express();
+	app.disable('x-powered-by');
+	app.use(bodyParser.json());
+	var router = require('./module/router')(app);
 
-	var r = router(app);
-
-	const checkPermission = (req, res, next) => {
-		var extensions = r.findRouteExtensions(req.method, req.path);
+	const checkPermission = (request, response, next) => {
+		var extensions = router.findRoute(request.method, request.route.path).extensions;
 
 		if ('permissions' in extensions && extensions.permissions.length !== 0) {
-			const applicationKey = req.get('X-Application-Key');
-			const accessKey = req.get('X-Access-Key');
+			const applicationKey = request.get('X-Application-Key');
+			const accessKey = request.get('X-Access-Key');
 
 			if (applicationKey === undefined) {
-				res.send({error: {message: 'X-Application-Key header is empty'}});
+				response.send({error: {message: 'X-Application-Key header is empty'}});
 				return;
 			}
 
 			if (accessKey === undefined) {
-				res.send({error: {message: 'X-Access-Key header is empty'}});
+				response.send({error: {message: 'X-Access-Key header is empty'}});
 				return;
 			}
+
+			// TODO: insert user/application object in request
+			request.user = {};
+			request.application = {};
 
 			// TODO: varify keys
 			// TODO: check permissions
@@ -40,8 +44,9 @@ module.exports = () => {
 		next();
 	};
 
-	r.addRoutes(routes(), [checkPermission]);
+	router.addRoutes(routes(), [checkPermission]);
 
-	app.listen(config.api.port);
-	console.log(`listen on port: ${config.api.port}`);
+	app.listen(config.api.port, () => {
+		console.log(`listen on port: ${config.api.port}`);
+	});
 }
