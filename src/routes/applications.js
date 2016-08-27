522afb79f4b9a7831ae9c5fdab2b6a186dd0152b
Marihachi <marihachi0620@gmail.com>
2016-08-27 21:57:29 +0900

    implement POST applications

--- src/routes/applications.js
+++ src/routes/applications.js
@@ -1,13 +1,37 @@
 'use strict';
 
 const apiResult = require('../modules/api-result');
+const dbConnector = require('../modules/db-connector')();
 const getMissingParams = require('../modules/get-missing-params');
+const type = require('../modules/type');
 
-exports.post = (params, extensions) => new Promise((resolve, reject) => (async () => {
-	const missingParams = getMissingParams(params, []);
-	if (missingParams.length) {
-		return reject(apiResult(400, 'some required parameters are missing', {target_params: missingParams}));
+exports.post = (request, extensions) => new Promise((resolve, reject) => (async () => {
+	const userId = request.user._id;
+	const name = request.body.name;
+	const description = request.body.description;
+	const permissions = request.body.permissions;
+
+	const db = await dbConnector.connectApidbAsync();
+
+	if (await db.findArrayAsync('applications', {name: name}).length >= 1)
+		return reject(apiResult(400, 'already exists name'));
+
+	// TODO: analyze permissions
+
+	let application;
+
+	try {
+		application = await db.createAsync('applications', {
+			name: name,
+			creator_id: userId,
+			description: description,
+			permissions: permissions
+		});
+	}
+	catch(err) {
+		console.log(err.stack);
+		return reject(apiResult(500, 'faild to create application'));
 	}
 
-	reject(apiResult(501, 'not implemented'));
+	return resolve(apiResult(200, null, {application: application}));
 })());
