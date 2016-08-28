66a220a7e089c9b23ac454a8eeaad89ea4585dab
Marihachi <marihachi0620@gmail.com>
2016-08-28 23:34:31 +0900

    [wip]implement document-models

--- src/document-models/user.js
+++ src/document-models/user.js
@@ -1,1 +1,12 @@
 'use strict';
+
+module.exports = (documentData, dbManager) => new Promise((resolve, reject) => (async () => {
+	const instance = {};
+
+	instance.documentData = documentData;
+	instance.dbManager = dbManager;
+
+	// TODO: 各種操作用メソッドの追加
+
+	return instance;
+})());
