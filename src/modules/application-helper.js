736a676d707b13be3c4dd055edd78ab33e57d19e
Marihachi <marihachi0620@gmail.com>
2016-08-30 05:53:45 +0900

    (kari)

--- src/modules/application-helper.js
+++ src/modules/application-helper.js
@@ -1,5 +1,13 @@
 'use strict';
 
 exports.analyzePermissions = () => {
+	// TODO
 
+	return true;
+};
+
+exports.buildApplicationKey = (applicationId, creatorId, keyCode) => {
+	// TODO
+
+	return "";
 };
