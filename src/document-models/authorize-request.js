6dc52dac6d5818ae16bc4e7891e28d96542fa923
Marihachi <marihachi0620@gmail.com>
2016-08-30 06:32:16 +0900

    implement

--- src/document-models/authorize-request.js
+++ src/document-models/authorize-request.js
@@ -19,11 +19,20 @@ module.exports = async (documentId, dbManager) => {
 	};
 
 	instance.generateRequestKey = async () => {
-		// TODO
+		const keyCode = randomRange(1, 99999);
+		dbManager.updateAsync('authorizeRequests', {_id: documentId}, {key_code: keyCode});
+		const request = await dbManager.findArrayAsync('authorizeRequests', {_id: documentId})[0];
+
+		return authorizeRequestHelper.buildRequestKey(request._id, request.application_id, request.key_code);
 	};
 
 	instance.getRequestKey = async () => {
-		// TODO
+		const request = await dbManager.findArrayAsync('authorizeRequests', {_id: documentId})[0];
+
+		if (request == undefined)
+			throw new Error('authorize-request not found');
+
+		return authorizeRequestHelper.buildRequestKey(request._id, request.application_id, request.key_code);
 	};
 
 	return instance;
