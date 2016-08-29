0ef6857cc72ab29bd9e355f44805abb0be6f4801
Marihachi <marihachi0620@gmail.com>
2016-08-30 05:27:31 +0900

    update

--- src/modules/db-manager.js
+++ src/modules/db-manager.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const log = require('./log');
-
 module.exports = (db) => {
 	const instance = {db: db};
 
@@ -11,10 +9,7 @@ module.exports = (db) => {
 	 * @param  {string} collectionName コレクション名
 	 * @param  {string} data
 	 */
-	var createAsync = (async (collectionName, data) => {
-		return await instance.db.collection(collectionName).insert(data);
-	});
-	instance.createAsync = createAsync;
+	instance.createAsync = async (collectionName, data) => await instance.db.collection(collectionName).insert(data);
 
 	/**
 	 * ドキュメントを検索します
@@ -24,11 +19,7 @@ module.exports = (db) => {
 	 * @param  {Object} projection
 	 * @param  {Object[]|null} documents
 	 */
-	var findArrayAsync = (async (collectionName, query, projection) => {
-		var items = await instance.db.collection(collectionName).find(query, projection);
-		return await items.toArray();
-	});
-	instance.findArrayAsync = findArrayAsync;
+	instance.findArrayAsync = async (collectionName, query, projection) => (await instance.db.collection(collectionName).find(query, projection)).toArray();
 
 	/**
 	 * ドキュメントを更新します
@@ -37,13 +28,12 @@ module.exports = (db) => {
 	 * @param  {Object} data
 	 * @param  {Boolean} isMany
 	 */
-	var updateAsync = (async (collectionName, data, isMany) => {
+	instance.updateAsync = async (collectionName, data, isMany) => {
 		if (isMany == undefined)
 			isMany = false;
 
 		return await instance.db.collection(collectionName).update(data, {$set: {data}}, !isMany, isMany);
-	});
-	instance.updateAsync = updateAsync;
+	};
 
 	/**
 	 * ドキュメントを削除します
@@ -51,10 +41,7 @@ module.exports = (db) => {
 	 * @param  {string} collectionName
 	 * @param  {Object} query
 	 */
-	var removeAsync = (async (collectionName, query) => {
-		return await instance.db.collection(collectionName).remove(query);
-	});
-	instance.removeAsync = removeAsync;
+	instance.removeAsync = async (collectionName, query) => await instance.db.collection(collectionName).remove(query);
 
 	return instance;
 }
