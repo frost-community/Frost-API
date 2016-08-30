c521791ddc2ea6761d5e2eb0c59c67e1d3187a49
Marihachi <marihachi0620@gmail.com>
2016-08-31 01:24:05 +0900

    add require

--- src/modules/authorize-request-helper.js
+++ src/modules/authorize-request-helper.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const crypto = require('crypto');
+const config = require('./load-config')();
+
 const buildRequestKeyHash = (authorizeRequestId, applicationId, keyCode) => {
 	const sha256 = crypto.createHash('sha256');
 	sha256.update(`${config.api.secret_token.authorize_request}/${applicationId}/${authorizeRequestId}/${keyCode}`);
