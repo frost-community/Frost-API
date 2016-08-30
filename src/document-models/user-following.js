c64c41f4dcf35ea84f5dd8c846d65d3732dfc723
Marihachi <marihachi0620@gmail.com>
2016-08-31 00:52:52 +0900

    rm

--- src/document-models/user-following.js
+++ src/document-models/user-following.js
@@ -3,9 +3,6 @@
 module.exports = async (documentId, dbManager) => {
 	const instance = {};
 
-	instance.documentId = documentId;
-	instance.dbManager = dbManager;
-
 	// TODO: 各種操作用メソッドの追加
 
 	return instance;
