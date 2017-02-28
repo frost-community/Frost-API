0c6aa2acb37431862303949e9ec4f4cac1102825
Marihachi <marihachi0620@gmail.com>
2017-02-28 19:20:38 +0900

    update

--- src/server.js
+++ src/server.js
@@ -24,21 +24,18 @@ module.exports = () => {
 		var extensions = router.findRoute(request.method, request.route.path).extensions;
 
 		if ('params' in extensions && extensions.params.length !== 0) {
-			for (var i = 0; i < extensions.params.length; i++) {
-				const param = extensions.params[i];
-
-				if (param.type == undefined || param.name == undefined)
-				{
+			for(var param of extensions.params) {
+				if (param.type == null || param.name == null) {
 					response.status(500).send({error: {message: 'internal error', details: 'extentions.params elements are missing'}});
 					throw new Error('extentions.params elements are missing');
 				}
 
 				const paramType = param.type;
 				const paramName = param.name;
-				const isRequire = param.require == undefined || param.require !== false; // requireにtrueが設定されている場合は必須項目になる。デフォルトでtrue
+				const isRequire = param.require == null || param.require !== false; // requireにtrueが設定されている場合は必須項目になる。デフォルトでtrue
 
 				if (isRequire) {
-					if (request.body[paramName] == undefined) {
+					if (request.body[paramName] == null) {
 						response.status(400).send({error: {message: `parameter '${paramName}' is require`}});
 						return;
 					}
@@ -61,12 +58,12 @@ module.exports = () => {
 			const applicationKey = request.get('X-Application-Key');
 			const accessKey = request.get('X-Access-Key');
 
-			if (applicationKey == undefined) {
+			if (applicationKey == null) {
 				response.status(400).send({error: {message: 'X-Application-Key header is empty'}});
 				return;
 			}
 
-			if (accessKey == undefined) {
+			if (accessKey == null) {
 				response.status(400).send({error: {message: 'X-Access-Key header is empty'}});
 				return;
 			}
@@ -88,7 +85,14 @@ module.exports = () => {
 			request.application = dbManager.findArrayAsync('applications', {_id: applicationId});
 			request.user = dbManager.findArrayAsync('users', {_id: userId});
 
-			for (var i = 0; i < request.application.permissions.length; i++) {
+			for (var permission of request.application.permissions) {
+				if (!request.application.isHasPermission(request.application.permissions[i])) {
+					response.status(403).send({error: {message: 'you don\'t have any permissions'}});
+					return;
+				}
+			}
+
+			for (var permission of request.application.permissions) {
 				if (!request.application.isHasPermission(request.application.permissions[i])) {
 					response.status(403).send({error: {message: 'you don\'t have any permissions'}});
 					return;
