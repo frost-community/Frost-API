0cabc9544991dd9673e6fa21410f78462a77357a
Marihachi <marihachi0620@gmail.com>
2017-03-04 12:28:29 +0900

    [wip]

--- src/routes/ice-auth/request.js
+++ src/routes/ice-auth/request.js
@@ -2,6 +2,11 @@
 
 const apiResult = require('../../helpers/api-result');
 
+const authorizeRequestsAsync = require('../../collections/authorize-requests');
+const authorizeRequestModel = require('../../models/authorize-request');
+
 exports.post = async (request, extensions) => {
-	throw apiResult(501, 'not implemented');
+	var doc = await (await authorizeRequestsAsync()).createAsync('appid'); // TODO
+
+	return apiResult(200, 'request creation successful', doc);
 };
