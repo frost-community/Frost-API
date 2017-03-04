2b8e252ba72f3ef4870230def30a9673dde856d7
Marihachi <marihachi0620@gmail.com>
2017-03-04 14:37:52 +0900

    ん

--- src/routes/ice-auth/authorize.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const apiResult = require('../../helpers/api-result');
-
-exports.post = async (request, extensions) => {
-	throw new Error(apiResult(501, 'not implemented'));
-};
---
+++ src/routes/iceAuth/authorize.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const apiResult = require('../../helpers/apiResult');
+
+exports.post = async (request, extensions) => {
+	throw apiResult(501, 'not implemented');
+};
