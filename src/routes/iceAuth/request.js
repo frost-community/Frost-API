7b0a4dca5b9c63455ecdb3693d7a8f9057954800
Marihachi <marihachi0620@gmail.com>
2017-03-04 16:44:53 +0900

    implement POST iceAuth/request

--- src/routes/iceAuth/request.js
+++ src/routes/iceAuth/request.js
@@ -3,12 +3,19 @@
 const apiResult = require('../../helpers/apiResult');
 
 const authorizeRequestsAsync = require('../../collections/authorizeRequests');
-const authorizeRequestModel = require('../../models/authorizeRequest');
+const applications = require('../../collections/application');
+
+const applicationModel = require('../../models/application');
 
 exports.post = async (request, extensions) => {
-	const description = request.body.applicationKey;
+	const applicationKey = request.body.applicationKey;
 
-	const doc = await (await authorizeRequestsAsync()).createAsync('appid'); // TODO
+	if (await applicationModel.verifyKeyAsync(applicationKey))
+		return apiResult(400, 'applicationKey is invalid');
 
-	return apiResult(200, 'request creation successful', doc);
+	const applicationId = applicationModel.splitKey(applicationKey).applicationId;
+	const application = await (await applications).findAsync(applicationId);
+	const key = await application.generateApplicationKeyAsync();
+
+	return apiResult(200, 'successful', {requestKey: key});
 };
