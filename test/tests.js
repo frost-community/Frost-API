db88ef071bbb07ebe6856ce9e611fe7e8413031a
Marihachi <marihachi0620@gmail.com>
2017-03-05 16:41:02 +0900

    add test

--- test/tests.js
+++ test/tests.js
@@ -5,8 +5,17 @@ const config = require('../built/helpers/loadConfig')();
 
 
 describe('routes', () => {
-	const routes = require('../built/routes');
-	it('権限リストにある権限名のみを利用している', (done) => {
+	const routes = require('../built/routes')();
+
+	it('存在するHTTPメソッドを利用している', () => {
+		for(const route of routes) {
+			let method = route[0];
+			method = method.replace(/^del$/, 'delete');
+			assert(require('methods').indexOf(method) > -1);
+		}
+	});
+
+	it('権限リストにある権限名のみを利用している', () => {
 		for(const route of routes) {
 			if ('permissions' in route[2]) {
 				for(const permission of route[2].permissions) {
@@ -17,9 +26,10 @@ describe('routes', () => {
 	});
 });
 
-describe('dbManager', () => {
+describe('DB', () => {
 	const db = require('../built/helpers/dbConnector')();
 	let testDb;
+
 	it('DBに接続してそのインスタンスが取得できる', (done) => {
 		(async () => {
 			try {
@@ -73,5 +83,16 @@ describe('dbManager', () => {
 				done(e);
 			}
 		})();
+	});
+});
+
+describe('randomRange', () => {
+	const random = require('../built/helpers/randomRange');
+
+	it('範囲を指定して生成した乱数の値はその範囲内にある', () => {
+		for(let i = 0; i < 1000; i++) {
+			const value = random(64, 1024);
+			assert(value >= 64 && value <= 1024);
+		}
 	});
 });
