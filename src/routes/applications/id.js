92ccd29bcca264b2e1b3305ebb7aa4f846c14b8a
Marihachi <marihachi0620@gmail.com>
2017-03-12 07:44:41 +0900

    update test

--- src/routes/applications/id.js
+++ src/routes/applications/id.js
@@ -6,9 +6,12 @@ const applications = require('../../collections/applications');
 exports.get = async (request, extensions, config) => {
 	const applicationDoc = await (await applications(config)).findIdAsync(request.params.id);
 
+	if (applicationDoc == null)
+		return apiResult(400, 'application is not found');
+
 	// 対象アプリケーションの所有者かどうかをチェック
 	if (applicationDoc.document.creatorId !== request.user.id)
-		return apiResult(400, 'you do not own this application.');
+		return apiResult(400, 'you do not own this application');
 
 	return apiResult(200, 'success', {application: applicationDoc.document});
 };
--- test/api/applications.js
+++ test/api/applications.js
@@ -159,31 +159,53 @@ describe('API', () => {
 	});
 
 	describe('GET  /applications/id', () => {
-		let account, app;
+		let accountA, appA, accountB, appB;
 		before(done => {
 			(async () => {
 				try {
 					let res = await routeAccount.post({
 						body: {
-							screenName: 'testuser',
+							screenName: 'testuser_a',
 							password: 'abcdefg',
 							name: 'froster',
 							description: 'this is testuser.'
 						}
 					}, null, config);
 					assert.equal(res.statusCode, 200);
-					account = res.data.user;
+					accountA = res.data.user;
+
+					res = await routeAccount.post({
+						body: {
+							screenName: 'testuser_b',
+							password: 'abcdefg',
+							name: 'froster',
+							description: 'this is testuser.'
+						}
+					}, null, config);
+					assert.equal(res.statusCode, 200);
+					accountB = res.data.user;
 
 					res = await routeApp.post({
-						user: {id: account.id},
+						user: {id: accountA.id},
 						body: {
-							name: 'testapp',
+							name: 'testapp_a',
 							description: 'this is testapp.',
 							permissions: []
 						}
 					}, null, config);
 					assert.equal(res.statusCode, 200);
-					app = res.data.application;
+					appA = res.data.application;
+
+					res = await routeApp.post({
+						user: {id: accountB.id},
+						body: {
+							name: 'testapp_b',
+							description: 'this is testapp.',
+							permissions: []
+						}
+					}, null, config);
+					assert.equal(res.statusCode, 200);
+					appB = res.data.application;
 
 					done();
 				}
@@ -211,8 +233,8 @@ describe('API', () => {
 			(async () => {
 				try {
 					let res = await routeAppId.get({
-						user: {id: account.id},
-						params: {id: app.id},
+						user: {id: accountA.id},
+						params: {id: appA.id},
 						body: {}
 					}, null, config);
 
@@ -220,13 +242,51 @@ describe('API', () => {
 
 					assert.deepEqual(res.data, {
 						application: {
-							id: app.id,
-							creatorId: account.id,
-							name: app.name,
-							description: app.description,
-							permissions: app.permissions
+							id: appA.id,
+							creatorId: accountA.id,
+							name: appA.name,
+							description: appA.description,
+							permissions: appA.permissions
 						}
 					});
+
+					done();
+				}
+				catch(e) {
+					done(e);
+				}
+			})();
+		});
+
+		it('所有していないアプリケーションを指定された場合は失敗する', done => {
+			(async () => {
+				try {
+					let res = await routeAppId.get({
+						user: {id: accountA.id},
+						params: {id: appB.id},
+						body: {}
+					}, null, config);
+
+					assert.equal(res.statusCode, 400);
+
+					done();
+				}
+				catch(e) {
+					done(e);
+				}
+			})();
+		});
+
+		it('存在しないアプリケーションを指定された場合は失敗する', done => {
+			(async () => {
+				try {
+					let res = await routeAppId.get({
+						user: {id: accountA.id},
+						params: {id: 'abcdefg1234'},
+						body: {}
+					}, null, config);
+
+					assert.equal(res.statusCode, 400);
 
 					done();
 				}
