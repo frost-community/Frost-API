468e6ce828a8733f1fc36752cb1055c6e1e92027
Marihachi <marihachi0620@gmail.com>
2017-03-14 22:44:25 +0900

    なんかもうめっちゃ変えた(2)

--- src/documentModels/userFollowing.js
+++ src/documentModels/userFollowing.js
@@ -6,15 +6,22 @@ const userFollowings = require('../helpers/collections').userFollowings;
 module.exports = async (document, config) => {
 	const instance = {};
 
-	// id加工
-	document.id = document._id.toString();
-	delete document._id;
-
 	instance.document = document;
 	instance.collection = await userFollowings(config);
 	//const userFollowingModel = await userFollowingModelAsync(config);
 
 	// TODO: 各種操作用メソッドの追加
+
+	instance.serialize = () => {
+		const res = {};
+		Object.assign(res, instance.document);
+
+		// id
+		res.id = res._id.toString();
+		delete res._id;
+
+		return res;
+	};
 
 	return instance;
 };
