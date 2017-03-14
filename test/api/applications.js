cc310598818a16699211f70331dfe73dcba4e132
Marihachi <marihachi0620@gmail.com>
2017-03-14 18:36:04 +0900

    update test

--- test/api/applications.js
+++ test/api/applications.js
@@ -2,19 +2,26 @@
 
 const assert = require('assert');
 const config = require('../../built/helpers/loadConfig')();
+const collections = require('../../built/helpers/collections');
+const usersAsync = collections.users;
+const applicationsAsync = collections.applications;
 const routeAccount = require('../../built/routes/account');
 const routeApp = require('../../built/routes/applications');
 const routeAppId = require('../../built/routes/applications/id');
 const routeAppIdApplicationKey = require('../../built/routes/applications/id/application_key');
-const dbConnector = require('../../built/helpers/dbConnector');
 
 describe('API', () => {
-	let dbManager;
+	// load collections
+	let users, applications;
 	before(done => {
 		(async () => {
 			try {
 				config.api.database = config.api.testDatabase;
-				dbManager = await dbConnector.connectApidbAsync(config);
+				users = await usersAsync(config);
+				applications = await applicationsAsync(config);
+
+				await users.removeAsync();
+				await applications.removeAsync();
 
 				done();
 			}
@@ -24,151 +31,18 @@ describe('API', () => {
 		})();
 	});
 
-	describe('POST /applications', () => {
-		let account;
-		before(done => {
-			(async () => {
-				try {
-					const res = await routeAccount.post({
-						body: {
-							screenName: 'testuser',
-							password: 'abcdefg',
-							name: 'froster',
-							description: 'this is testuser.'
-						}
-					}, null, config);
-					assert.equal(res.statusCode, 200);
-					account = res.data.user;
-
-					done();
-				}
-				catch(e) {
-					done(e);
-				}
-			})();
-		});
-
-		afterEach(done => {
-			(async () => {
-				try {
-					await dbManager.removeAsync('applications', {});
-
-					done();
-				}
-				catch(e) {
-					done(e);
-				}
-			})();
-		});
-
-		after(done => {
-			(async () => {
-				try {
-					await dbManager.removeAsync('users', {});
-
-					done();
-				}
-				catch(e) {
-					done(e);
-				}
-			})();
-		});
-
-		it('正しくリクエストされた場合は成功する', done => {
-			(async () => {
-				try {
-					let res = await routeApp.post({
-						user: {id: account.id},
-						body: {
-							name: 'hoge',
-							description: 'hogehoge',
-							permissions: []
-						}
-					}, null, config);
-
-					assert.equal(res.statusCode, 200);
-
-					delete res.data.application.id;
-					assert.deepEqual(res.data, {
-						application: {
-							name: 'hoge',
-							creatorId: account.id,
-							description: 'hogehoge',
-							permissions: []
-						}
-					});
-					done();
-				}
-				catch(e) {
-					done(e);
-				}
-			})();
-		});
-
-		it('nameが空もしくは33文字以上の場合は失敗する', done => {
-			(async () => {
-				try {
-					let res = await routeApp.post({
-						user: {id: account.id},
-						body: {
-							name: '',
-							description: 'hogehoge',
-							permissions: ''
-						}
-					}, null, config);
-					assert.equal(res.statusCode, 400);
-
-					res = await routeApp.post({
-						user: {id: account.id},
-						body: {
-							name: 'superFrostersuperFrostersuperFros',
-							description: 'hogehoge',
-							permissions: ''
-						}
-					}, null, config);
-					assert.equal(res.statusCode, 400);
-
-					done();
-				}
-				catch(e) {
-					done(e);
-				}
-			})();
-		});
-
-		it('descriptionが257文字以上のときは失敗する', done => {
-			(async () => {
-				try {
-					let res = await routeApp.post({
-						user: {id: account.id},
-						body: {
-							name: 'hoge',
-							description: 'testhogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthoget',
-							permissions: ''
-						}
-					}, null, config);
-					assert.equal(res.statusCode, 400);
-
-					done();
-				}
-				catch(e) {
-					done(e);
-				}
-			})();
-		});
-	});
-
-	describe('GET  /applications/id', () => {
-		let accountA, appA, accountB, appB;
+	describe('/applications', () => {
+		// add general users, general applications
+		let accountA, accountB, appA, appB;
 		before(done => {
 			(async () => {
 				try {
 					let res = await routeAccount.post({
 						body: {
-							screenName: 'testuser_a',
+							screenName: 'generaluser_a',
 							password: 'abcdefg',
 							name: 'froster',
-							description: 'this is testuser.'
+							description: 'this is generaluser.'
 						}
 					}, null, config);
 					assert.equal(res.statusCode, 200);
@@ -176,10 +50,10 @@ describe('API', () => {
 
 					res = await routeAccount.post({
 						body: {
-							screenName: 'testuser_b',
+							screenName: 'generaluser_b',
 							password: 'abcdefg',
 							name: 'froster',
-							description: 'this is testuser.'
+							description: 'this is generaluser.'
 						}
 					}, null, config);
 					assert.equal(res.statusCode, 200);
@@ -188,8 +62,8 @@ describe('API', () => {
 					res = await routeApp.post({
 						user: {id: accountA.id},
 						body: {
-							name: 'testapp_a',
-							description: 'this is testapp.',
+							name: 'generalapp_a',
+							description: 'this is generalapp.',
 							permissions: []
 						}
 					}, null, config);
@@ -199,8 +73,8 @@ describe('API', () => {
 					res = await routeApp.post({
 						user: {id: accountB.id},
 						body: {
-							name: 'testapp_b',
-							description: 'this is testapp.',
+							name: 'generalapp_b',
+							description: 'this is generalapp.',
 							permissions: []
 						}
 					}, null, config);
@@ -215,11 +89,12 @@ describe('API', () => {
 			})();
 		});
 
+		// remove general users, general applications
 		after(done => {
 			(async () => {
 				try {
-					await dbManager.removeAsync('users', {});
-					await dbManager.removeAsync('applications', {});
+					await users.removeAsync({screenName: /general/});
+					await applications.removeAsync({screenName: /general/});
 
 					done();
 				}
@@ -229,113 +104,214 @@ describe('API', () => {
 			})();
 		});
 
-		it('正しくリクエストされた場合は成功する', done => {
-			(async () => {
-				try {
-					let res = await routeAppId.get({
-						user: {id: accountA.id},
-						params: {id: appA.id},
-						body: {}
-					}, null, config);
+		describe('POST', () => {
+			// general以外のアプリケーションを削除
+			afterEach(done => {
+				(async () => {
+					try {
+						await applications.removeAsync({screenName: /^(general)/});
 
-					assert.equal(res.statusCode, 200);
+						done();
+					}
+					catch(e) {
+						done(e);
+					}
+				})();
+			});
 
-					assert.deepEqual(res.data, {
-						application: {
-							id: appA.id,
-							creatorId: accountA.id,
-							name: appA.name,
-							description: appA.description,
-							permissions: appA.permissions
-						}
-					});
+			it('正しくリクエストされた場合は成功する', done => {
+				(async () => {
+					try {
+						let res = await routeApp.post({
+							user: {id: accountA.id},
+							body: {
+								name: 'temp',
+								description: 'hogehoge',
+								permissions: []
+							}
+						}, null, config);
 
-					done();
-				}
-				catch(e) {
-					done(e);
-				}
-			})();
+						assert.equal(res.statusCode, 200);
+
+						delete res.data.application.id;
+						assert.deepEqual(res.data, {
+							application: {
+								name: 'temp',
+								creatorId: accountA.id,
+								description: 'hogehoge',
+								permissions: []
+							}
+						});
+						done();
+					}
+					catch(e) {
+						done(e);
+					}
+				})();
+			});
+
+			it('nameが空もしくは33文字以上の場合は失敗する', done => {
+				(async () => {
+					try {
+						let res = await routeApp.post({
+							user: {id: accountA.id},
+							body: {
+								name: '',
+								description: 'hogehoge',
+								permissions: ''
+							}
+						}, null, config);
+						assert.equal(res.statusCode, 400);
+
+						res = await routeApp.post({
+							user: {id: accountA.id},
+							body: {
+								name: 'superFrostersuperFrostersuperFros',
+								description: 'hogehoge',
+								permissions: ''
+							}
+						}, null, config);
+						assert.equal(res.statusCode, 400);
+
+						done();
+					}
+					catch(e) {
+						done(e);
+					}
+				})();
+			});
+
+			it('descriptionが257文字以上のときは失敗する', done => {
+				(async () => {
+					try {
+						let res = await routeApp.post({
+							user: {id: accountA.id},
+							body: {
+								name: 'temp',
+								description: 'testhogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthogetesthoget',
+								permissions: ''
+							}
+						}, null, config);
+						assert.equal(res.statusCode, 400);
+
+						done();
+					}
+					catch(e) {
+						done(e);
+					}
+				})();
+			});
 		});
 
-		it('所有していないアプリケーションを指定された場合は失敗する', done => {
-			(async () => {
-				try {
-					let res = await routeAppId.get({
-						user: {id: accountA.id},
-						params: {id: appB.id},
-						body: {}
-					}, null, config);
+		describe('GET  /applications/id', () => {
+			it('正しくリクエストされた場合は成功する', done => {
+				(async () => {
+					try {
+						let res = await routeAppId.get({
+							user: {id: accountA.id},
+							params: {id: appA.id},
+							body: {}
+						}, null, config);
 
-					assert.equal(res.statusCode, 400);
+						assert.equal(res.statusCode, 200);
 
-					done();
-				}
-				catch(e) {
-					done(e);
-				}
-			})();
+						assert.deepEqual(res.data, {
+							application: {
+								id: appA.id,
+								creatorId: accountA.id,
+								name: appA.name,
+								description: appA.description,
+								permissions: appA.permissions
+							}
+						});
+
+						done();
+					}
+					catch(e) {
+						done(e);
+					}
+				})();
+			});
+
+			it('所有していないアプリケーションを指定された場合は失敗する', done => {
+				(async () => {
+					try {
+						let res = await routeAppId.get({
+							user: {id: accountA.id},
+							params: {id: appB.id},
+							body: {}
+						}, null, config);
+
+						assert.equal(res.statusCode, 400);
+
+						done();
+					}
+					catch(e) {
+						done(e);
+					}
+				})();
+			});
+
+			it('存在しないアプリケーションを指定された場合は失敗する', done => {
+				(async () => {
+					try {
+						let res = await routeAppId.get({
+							user: {id: accountA.id},
+							params: {id: 'abcdefg1234'},
+							body: {}
+						}, null, config);
+
+						assert.equal(res.statusCode, 400);
+
+						done();
+					}
+					catch(e) {
+						done(e);
+					}
+				})();
+			});
 		});
 
-		it('存在しないアプリケーションを指定された場合は失敗する', done => {
-			(async () => {
-				try {
-					let res = await routeAppId.get({
-						user: {id: accountA.id},
-						params: {id: 'abcdefg1234'},
-						body: {}
-					}, null, config);
+		describe('POST /applications/id/application_key', () => {
+			it('正しくリクエストされた場合は成功する', done => {
+				(async () => {
+					try {
+						let res = await routeAppIdApplicationKey.post({params: {id: 'application_id_hoge'}}, null, config);
 
-					assert.equal(res.statusCode, 400);
+						assert.equal(res.statusCode, 200);
 
-					done();
-				}
-				catch(e) {
-					done(e);
-				}
-			})();
+						assert.deepEqual(res.data, {
+							application_key: 'application_key_hoge'
+						});
+
+						done();
+					}
+					catch(e) {
+						done(e);
+					}
+				})();
+			});
 		});
-	});
 
-	describe('POST /applications/id/application_key', () => {
-		it('正しくリクエストされた場合は成功する', done => {
-			(async () => {
-				try {
-					let res = await routeAppIdApplicationKey.post({params: {id: 'application_id_hoge'}}, null, config);
+		describe('GET  /applications/id/application_key', () => {
+			it('正しくリクエストされた場合は成功する', done => {
+				(async () => {
+					try {
+						let res = await routeAppIdApplicationKey.get({params: {id: 'application_id_hoge'}}, null, config);
 
-					assert.equal(res.statusCode, 200);
+						assert.equal(res.statusCode, 200);
 
-					assert.deepEqual(res.data, {
-						application_key: 'application_key_hoge'
-					});
+						assert.deepEqual(res.data, {
+							application_key: 'application_key_hoge'
+						});
 
-					done();
-				}
-				catch(e) {
-					done(e);
-				}
-			})();
-		});
-	});
-
-	describe('GET  /applications/id/application_key', () => {
-		it('正しくリクエストされた場合は成功する', done => {
-			(async () => {
-				try {
-					let res = await routeAppIdApplicationKey.get({params: {id: 'application_id_hoge'}}, null, config);
-
-					assert.equal(res.statusCode, 200);
-
-					assert.deepEqual(res.data, {
-						application_key: 'application_key_hoge'
-					});
-
-					done();
-				}
-				catch(e) {
-					done(e);
-				}
-			})();
+						done();
+					}
+					catch(e) {
+						done(e);
+					}
+				})();
+			});
 		});
 	});
 });
