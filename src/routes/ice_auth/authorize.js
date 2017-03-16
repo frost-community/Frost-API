d2c71c3657a4d36d4a99e406185be1d1120312ba
Marihachi <marihachi0620@gmail.com>
2017-03-16 17:48:03 +0900

    [wip]implement routes

--- src/routes/ice_auth/authorize.js
+++ src/routes/ice_auth/authorize.js
@@ -1,7 +1,41 @@
 'use strict';
 
 const apiResult = require('../../helpers/apiResult');
+const applicationModelAsync = require('../../models/application');
+const authorizeRequestsModelAsync = require('../../models/authorizeRequest');
+const applicationAccessModelAsync = require('../../models/applicationAccess');
 
 exports.post = async (request, extensions, db, config) => {
-	return apiResult(501, 'not implemented');
+	const applicationKey = request.body.application_key;
+	const requestKey = request.body.request_key;
+	const verificationCode = request.body.verification_code;
+
+	const applicationModel = await applicationModelAsync(db, config);
+	if (!await applicationModel.verifyKeyAsync(applicationKey))
+		return apiResult(400, 'application_key is invalid');
+
+	const authorizeRequestsModel = await authorizeRequestsModelAsync(db, config);
+	if (!await authorizeRequestsModel.verifyKeyAsync(requestKey))
+		return apiResult(400, 'request_key is invalid');
+
+	const applicationId = applicationModel.splitKey(applicationKey).applicationId;
+	const authorizeRequestId = authorizeRequestsModel.splitKey(requestKey).authorizeRequestId;
+
+	const doc = await db.authorizeRequests.findAsync({_id: authorizeRequestId, applicationId: applicationId});
+
+	if (doc == null)
+		return apiResult(400, 'combination of application_key and request_key is invalid');
+
+	if (verificationCode !== doc.document.verificationCode)
+		return apiResult(400, 'verification_code is invalid');
+
+	// TODO: userIdどうすっかな～
+
+	await db.applicationAccesses.createAsync({
+		applicationId: applicationId,
+		userId: null,
+		keyCode: null
+	});
+
+	return apiResult(200, 'success', {'access_key': null});
 };
--- src/routes/ice_auth/verification_code.js
+++ src/routes/ice_auth/verification_code.js
@@ -9,11 +9,10 @@ exports.get = async (request, extensions, db, config) => {
 	const requestKey = request.body.request_key;
 
 	const applicationModel = await applicationModelAsync(db, config);
-	const authorizeRequestsModel = await authorizeRequestsModelAsync(db, config);
-
 	if (!await applicationModel.verifyKeyAsync(applicationKey))
 		return apiResult(400, 'application_key is invalid');
 
+	const authorizeRequestsModel = await authorizeRequestsModelAsync(db, config);
 	if (!await authorizeRequestsModel.verifyKeyAsync(requestKey))
 		return apiResult(400, 'request_key is invalid');
 
@@ -21,6 +20,9 @@ exports.get = async (request, extensions, db, config) => {
 	const authorizeRequestId = authorizeRequestsModel.splitKey(requestKey).authorizeRequestId;
 
 	const doc = await db.authorizeRequests.findAsync({_id: authorizeRequestId, applicationId: applicationId});
+
+	if (doc == null)
+		return apiResult(400, 'combination of application_key and request_key is invalid');
 
 	return apiResult(200, 'success', {'verification_code': doc.document.verificationCode});
 };
