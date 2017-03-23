ae7097271554a75545ace089d1f6eefd5883eb85
Marihachi <marihachi0620@gmail.com>
2017-03-24 04:19:33 +0900

    update

--- src/helpers/permission.js
+++ src/helpers/permission.js
@@ -1,14 +1,14 @@
 'use strict';
 
 exports.permissionTypes = [
-	'iceAuthHost',       // 認証のホスト権限
-	'application',         // 連携アプリ操作
+	'iceAuthHost',        // 認証のホスト権限
+	'application',        // 連携アプリ操作
 	'applicationSpecial', // 連携アプリ特殊操作
 	'accountRead',        // アカウント情報の取得
 	'accountWrite',       // アカウント情報の変更
 	'accountSpecial',     // アカウント情報の特殊操作
 	'userRead',           // ユーザー情報の取得
 	'userWrite',          // ユーザーのフォロー等のアクション
-	'postRead',           // 投稿の取得
-	'postWrite',          // 投稿の作成や削除等のアクション
+	'postRead',           // 投稿やリアクションの取得
+	'postWrite',          // 投稿やリアクションの作成・削除等のアクション
 ];
