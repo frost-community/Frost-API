132778501983494d0bee21045d8cf8bae9c9e5ea
Marihachi <marihachi0620@gmail.com>
2017-04-04 03:54:47 +0900

    update config

--- src/helpers/loadConfig.js
+++ src/helpers/loadConfig.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const fs = require('fs');
-
 /**
  * 設定ファイルを読み込みます。
  * リポジトリ直下ディレクトリか、その１つ上層のディレクトリからconfig.jsonを読み込むことができます。
@@ -10,10 +8,15 @@ const fs = require('fs');
  * @return {Object} JSONデータのパース結果
  */
 module.exports = () => {
-	if (fs.existsSync(`${process.cwd()}/config.json`))
+	try {
 		return require(`${process.cwd()}/config.json`);
-	else if (fs.existsSync(`${process.cwd()}/../config.json`))
-		return require(`${process.cwd()}/../config.json`);
-	else
-		return null;
+	}
+	catch(e) {
+		try {
+			return require(`${process.cwd()}/../config.json`);
+		}
+		catch(e2) {
+			return null;
+		}
+	}
 };
