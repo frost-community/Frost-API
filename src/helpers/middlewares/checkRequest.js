a9af6a3b83a6f5b103c5a414089c9873e73bbfc6
Marihachi <marihachi0620@gmail.com>
2017-05-07 20:47:40 +0900

    support for using as non-middleware (reverted from commit 8b9a7fb86e7bf8e3bc5ca0642ce8ea7da498d826)

--- src/helpers/middlewares/checkRequest.js
+++ src/helpers/middlewares/checkRequest.js
@@ -6,8 +6,6 @@ const Application = require('../../documentModels/application');
 const ApplicationAccess = require('../../documentModels/applicationAccess');
 
 module.exports = (request, response, next) => {
-	const isMiddleware = next != null;
-
 	request.checkRequestAsync = async (routeConfig) => {
 		try {
 			if (routeConfig == null)
@@ -27,36 +25,12 @@ module.exports = (request, response, next) => {
 				const isRequire = param.require != null ? param.require === true : true; // requireにtrueが設定されている場合は必須項目になる。デフォルトでtrue
 
 				if (isRequire) {
-					if (request.params[paramName] == null) {
+					if (request.body[paramName] == null) {
 						throw new apiResult(400, `parameter '${paramName}' is require`);
 					}
 
-					if (type(request.params[paramName]).toLowerCase() !== paramType.toLowerCase()) {
-						throw new apiResult(400, `type of parameter '${paramName}' is invalid`);
-					}
-				}
-			}
-
-			// body
-			if (routeConfig.body == null)
-				routeConfig.body = [];
-
-			for(const param of routeConfig.body) {
-				if (param.type == null || param.name == null) {
-					throw new Error('extentions.body elements are missing');
-				}
-
-				const paramType = param.type;
-				const paramName = param.name;
-				const isRequire = param.require != null ? param.require === true : true; // requireにtrueが設定されている場合は必須項目になる。デフォルトでtrue
-
-				if (isRequire) {
-					if (request.body[paramName] == null) {
-						throw new apiResult(400, `body parameter '${paramName}' is require`);
-					}
-
 					if (type(request.body[paramName]).toLowerCase() !== paramType.toLowerCase()) {
-						throw new apiResult(400, `type of body parameter '${paramName}' is invalid`);
+						throw new apiResult(400, `type of parameter '${paramName}' is invalid`);
 					}
 				}
 			}
@@ -75,18 +49,11 @@ module.exports = (request, response, next) => {
 				const isRequire = query.require != null ? query.require === true : true; // requireにtrueが設定されている場合は必須項目になる。デフォルトでtrue
 
 				if (isRequire) {
-					let requestQuery = null;
-					try {
-						requestQuery = request.query[queryName];
-					}
-					catch(e) {
-						/* noop */
-					}
-					if (requestQuery == null) {
+					if (request.query[queryName] == null) {
 						throw new apiResult(400, `query '${queryName}' is require`);
 					}
 
-					if (type(requestQuery).toLowerCase() !== queryType.toLowerCase()) {
+					if (type(request.query[queryName]).toLowerCase() !== queryType.toLowerCase()) {
 						throw new apiResult(400, `type of query '${queryName}' is invalid`);
 					}
 				}
@@ -97,32 +64,30 @@ module.exports = (request, response, next) => {
 				routeConfig.permissions = [];
 
 			if (routeConfig.permissions.length !== 0) {
-				if (isMiddleware) {
-					const applicationKey = request.get('X-Application-Key');
-					const accessKey = request.get('X-Access-Key');
+				const applicationKey = request.get('X-Application-Key');
+				const accessKey = request.get('X-Access-Key');
 
-					if (applicationKey == null) {
-						throw new apiResult(400, 'X-Application-Key header is empty');
-					}
-
-					if (accessKey == null) {
-						throw new apiResult(400, 'X-Access-Key header is empty');
-					}
-
-					if (!await Application.verifyKeyAsync(applicationKey, request.db, request.config)) {
-						throw new apiResult(400, 'X-Application-Key header is invalid');
-					}
-
-					if (!await ApplicationAccess.verifyKeyAsync(accessKey, request.db, request.config)) {
-						throw new apiResult(400, 'X-Access-Key header is invalid');
-					}
-
-					const applicationId = Application.splitKey(applicationKey, request.db, request.config).applicationId;
-					const userId = ApplicationAccess.splitKey(accessKey, request.db, request.config).userId;
-
-					request.application = (await request.db.applications.findByIdAsync(applicationId));
-					request.user = (await request.db.users.findByIdAsync(userId));
+				if (applicationKey == null) {
+					throw new apiResult(400, 'X-Application-Key header is empty');
 				}
+
+				if (accessKey == null) {
+					throw new apiResult(400, 'X-Access-Key header is empty');
+				}
+
+				if (!await Application.verifyKeyAsync(applicationKey, request.db, request.config)) {
+					throw new apiResult(400, 'X-Application-Key header is invalid');
+				}
+
+				if (!await ApplicationAccess.verifyKeyAsync(accessKey, request.db, request.config)) {
+					throw new apiResult(400, 'X-Access-Key header is invalid');
+				}
+
+				const applicationId = Application.splitKey(applicationKey, request.db, request.config).applicationId;
+				const userId = ApplicationAccess.splitKey(accessKey, request.db, request.config).userId;
+
+				request.application = (await request.db.applications.findByIdAsync(applicationId));
+				request.user = (await request.db.users.findByIdAsync(userId));
 
 				const hasPermissions = routeConfig.permissions.every(p => request.application.hasPermission(p));
 				if (!hasPermissions) {
@@ -134,9 +99,6 @@ module.exports = (request, response, next) => {
 			if (routeConfig.headers == null)
 				routeConfig.headers = [];
 
-			if (request.headers == null)
-				request.headers = [];
-
 			if (routeConfig.headers.indexOf('X-Api-Version') == -1)
 				routeConfig.headers.push('X-Api-Version');
 
@@ -145,32 +107,16 @@ module.exports = (request, response, next) => {
 					throw new Error('extentions.headers elements are missing');
 				}
 
-				if (isMiddleware) {
-					if (request.get(header) == null) {
-						throw new apiResult(400, `${header} header is empty`);
-					}
+				if (request.get(header) == null) {
+					throw new apiResult(400, `${header} header is empty`);
 				}
-				else {
-					let requestHeader = null;
-					try {
-						requestHeader = request.headers[header.toLowerCase()];
-					}
-					catch(e) {
-						/* noop */
-					}
-
-					if (requestHeader == null) {
-						throw new apiResult(400, `${header} header is empty`);
-					}
-				}
-
 			}
 
 			return null;
 		}
 		catch(e) {
 			if (e instanceof Error) {
-				console.log(`checkRequest failed: ${e}`);
+				console.log(`checkRequest failed: ${e.trace}`);
 				throw e;
 			}
 			else {
@@ -179,6 +125,5 @@ module.exports = (request, response, next) => {
 		}
 	};
 
-	if (isMiddleware)
-		next();
+	next();
 };
