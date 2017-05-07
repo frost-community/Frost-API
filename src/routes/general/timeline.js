9bbbbbf23a0227b6893c4e48d6e39c3ab5ae5661
Marihachi <marihachi0620@gmail.com>
2017-05-08 01:07:33 +0900

    [wip]implement #63

--- src/routes/general/timeline.js
+++ src/routes/general/timeline.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const ApiResult = require('../../helpers/apiResult');
+const Post = require('../../documentModels/post');
 
 exports.get = async (request) => {
 	const result = await request.checkRequestAsync({
@@ -11,5 +12,16 @@ exports.get = async (request) => {
 	if (result != null)
 		return result;
 
-	return new ApiResult(501, 'not implemented');
+	let posts;
+	try {
+		posts = await Post.findArrayByTypeAsync({$in: ['status']}, true, 30, request.db, request.config);
+	}
+	catch(err) {
+		// noop
+	}
+
+	if (posts == null || posts.length == 0)
+		return new ApiResult(404, 'posts are empty');
+
+	return new ApiResult(200, {posts: posts.map(i => i.serialize())});
 };
