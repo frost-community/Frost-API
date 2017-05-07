da1701ba6bc1fcf95ec30401145ae49345119d8f
Marihachi <marihachi0620@gmail.com>
2017-05-07 13:18:19 +0900

    implement #20

--- src/routes/posts/post_status.js
+++ src/routes/posts/post_status.js
@@ -4,12 +4,34 @@ const ApiResult = require('../../helpers/apiResult');
 
 exports.post = async (request) => {
 	const result = await request.checkRequestAsync({
-		params: [],
+		params: [
+			{name: 'text', type: 'string'}
+		],
 		permissions: ['postWrite']
 	});
 
 	if (result != null)
 		return result;
 
-	return new ApiResult(501, 'not implemented');
+	const userId = request.user.document._id;
+	const text = request.body.text;
+
+	if (!/^\S{1,256}$/.test(text))
+		return new ApiResult(400, 'text is invalid format. max length is 256');
+
+	let postStatus;
+	try {
+		postStatus = await request.db.posts.createAsync({ // TODO: move to document models
+			userId: userId,
+			text: text
+		});
+	}
+	catch(err) {
+		console.log(err.trace);
+	}
+
+	if (postStatus == null)
+		return new ApiResult(500, 'faild to create postStatus');
+
+	return new ApiResult(200, {postStatus: postStatus.serialize()});
 };
