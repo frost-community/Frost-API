cf3276d8676df605c699c88548570fc6eb8cd46a
Marihachi <marihachi0620@gmail.com>
2017-07-09 05:08:22 +0900

    implement #16, #24

--- src/routes/users/id/follow.js
+++ src/routes/users/id/follow.js
@@ -2,6 +2,7 @@
 
 const ApiResult = require('../../../helpers/apiResult');
 const User = require('../../../documentModels/user');
+const UserFollowing = require('../../../documentModels/userFollowing');
 
 exports.post = async (request) => {
 	const result = await request.checkRequestAsync({
@@ -13,21 +14,46 @@ exports.post = async (request) => {
 		return result;
 	}
 
-	const user = await User.findByIdAsync(request.params.id, request.db, request.config);
+	// me
+	const meId = request.user.document._id;
 
-	if (user == null) {
-		return new ApiResult(404, 'user is not found');
+	// targetUser
+	const targetUser = await User.findByIdAsync(request.params.id, request.db, request.config);
+	if (targetUser == null) {
+		return new ApiResult(404, 'target user is not found');
+	}
+	const targetUserId = targetUser.document._id;
+
+	// message
+	const message = request.body.message;
+	if (message != null && (/^\s*$/.test(message) || /^[\s\S]{1,64}$/.test(message) == false)) {
+		return new ApiResult(400, 'message is invalid format.');
 	}
 
-	const userId = user.document._id.toString();
+	// ドキュメント作成
+	let userFollowing;
+	try {
+		userFollowing = await request.db.userFollowings.createAsync({ // TODO: move to document models
+			source: meId,
+			destination: targetUserId,
+			message: message
+		});
+	}
+	catch(err) {
+		console.log(err.trace);
+	}
+
+	if (userFollowing == null) {
+		return new ApiResult(500, 'failed to create userFollowing');
+	}
 
 	// 購読
-	const subscriber = request.subscribers.get(userId);
-	if (subscriber != null) {
-		subscriber.subscribe(`${userId}:status`);
+	const meSubscriber = request.subscribers.get(meId.toString());
+	if (meSubscriber != null) {
+		meSubscriber.subscribe(`${targetUserId.toString()}:status`);
 	}
 
-	return new ApiResult(501, 'not implemented');
+	return new ApiResult(200, {userFollowing: userFollowing.serialize()});
 };
 
 exports.del = async (request) => {
@@ -40,19 +66,29 @@ exports.del = async (request) => {
 		return result;
 	}
 
-	const user = await User.findByIdAsync(request.params.id, request.db, request.config);
+	// me
+	const meId = request.user.document._id;
 
-	if (user == null) {
-		return new ApiResult(404, 'user is not found');
+	// targetUser
+	const targetUser = await User.findByIdAsync(request.params.id, request.db, request.config);
+	if (targetUser == null) {
+		return new ApiResult(404, 'target user is not found');
+	}
+	const targetUserId = targetUser.document._id;
+
+	const userFollowing = await UserFollowing.findBySrcDestAsync(meId, targetUserId, request.db, request.config);
+
+	if (userFollowing == null) {
+		return new ApiResult(400, 'you are not following this user');
 	}
 
-	const userId = user.document._id.toString();
+	await userFollowing.removeAsync();
 
 	// 購読解除
-	const subscriber = request.subscribers.get(userId);
+	const subscriber = request.subscribers.get(meId.toString());
 	if (subscriber != null) {
-		subscriber.unsubscribe(`${userId}:status`);
+		subscriber.unsubscribe(`${targetUserId.toString()}:status`);
 	}
 
-	return new ApiResult(501, 'not implemented');
+	return new ApiResult(200);
 };
