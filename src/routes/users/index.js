e01504db99734a5eda199f7662402e431585331b
Marihachi <marihachi0620@gmail.com>
2017-07-22 22:44:17 +0900

    allow to fetch all user

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -16,21 +16,23 @@ exports.get = async (request) => {
 		return result;
 	}
 
-	const screenNames = request.query.screen_names.split(',');
+	let users;
+	if (request.query.screen_names != null) {
+		const screenNames = request.query.screen_names.split(',');
 
-	if (screenNames.lenth == 0) {
-		return new ApiResult(400, 'screen_names query is empty');
+		if (screenNames.lenth > 100) {
+			return new ApiResult(400, 'screen_names query is limit over(100 items or less)');
+		}
+
+		if (screenNames.every(i => User.checkFormatScreenName(i)) === false) {
+			return new ApiResult(400, 'screen_names query is invalid');
+		}
+
+		users = await User.findArrayByScreenNamesAsync(screenNames, null, request.db, request.config);
 	}
-
-	if (screenNames.lenth > 100) {
-		return new ApiResult(400, 'screen_names query is limit over(100 items or less)');
+	else {
+		users = await request.db.users.findArrayAsync({});
 	}
-
-	if (screenNames.every(i => User.checkFormatScreenName(i)) === false) {
-		return new ApiResult(400, 'screen_names query is invalid');
-	}
-
-	const users = await User.findArrayByScreenNamesAsync(screenNames, null, request.db, request.config);
 
 	if (users == null || users.length == 0) {
 		return new ApiResult(204);
