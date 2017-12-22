d9741d9618d8fe64fbdd1a28a6217761c0021bb7
marihachi <marihachi0620@gmail.com>
2017-12-23 08:52:56 +0900

    [wip] improve streaming

--- src/helpers/StreamingHelpers.js
+++ src/helpers/StreamingHelpers.js
@@ -2,7 +2,11 @@ const { Application } = require('../documentModels/application');
 const { ApplicationAccess } = require('../documentModels/applicationAccess');
 
 class StreamingHelpers {
-	static async checkRequest (request, applicationKey, accessKey, db, config) {
+	static async checkRequest (request, db, config) {
+		const query = request.resourceURL.query;
+		const applicationKey = query.application_key;
+		const accessKey = query.access_key;
+
 		if (applicationKey == null) {
 			const message = 'application_key parameter is empty';
 			request.reject(400, message);
@@ -28,7 +32,6 @@ class StreamingHelpers {
 		}
 
 		return {
-			connection: request.accept(),
 			applicationId: Application.splitKey(applicationKey, db, config).applicationId,
 			meId: ApplicationAccess.splitKey(accessKey, db, config).userId
 		};
