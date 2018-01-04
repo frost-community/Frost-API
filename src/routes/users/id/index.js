dae959896adc34d7a3485a50f97f70359e4908ad
marihachi <marihachi0620@gmail.com>
2018-01-05 00:25:43 +0900

    add parameter followingsCount and followersCount on /users/:id

--- src/routes/users/id/index.js
+++ src/routes/users/id/index.js
@@ -1,4 +1,5 @@
 const User = require('../../../documentModels/user');
+const UserFollowing = require('../../../documentModels/userFollowing');
 // const $ = require('cafy').default;
 
 exports.get = async (apiContext) => {
@@ -9,11 +10,18 @@ exports.get = async (apiContext) => {
 	if (apiContext.responsed) return;
 
 	const user = await User.findByIdAsync(apiContext.params.id, apiContext.db, apiContext.config);
-
 	if (user == null) {
 		apiContext.response(204);
 		return;
 	}
+	const serialized = user.serialize();
 
-	apiContext.response(200, { user: user.serialize() });
+	let [followings, followers] = await Promise.all([
+		UserFollowing.findTargetsAsync(user.document._id, 1000, apiContext.db, apiContext.config),
+		UserFollowing.findSourcesAsync(user.document._id, 1000, apiContext.db, apiContext.config)
+	]);
+	serialized.followingsCount = (followings || []).length;
+	serialized.followersCount = (followers || []).length;
+
+	apiContext.response(200, { user: serialized });
 };
