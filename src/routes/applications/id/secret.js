ef3e8f93924fc2a4d36922580b145ac39ee7b6eb
marihachi <marihachi0620@gmail.com>
2018-03-26 06:06:13 +0900

    app secret

---
+++ src/routes/applications/id/secret.js
@@ -0,0 +1,44 @@
+const ApiContext = require('../../../modules/ApiContext');
+// const $ = require('cafy').default;
+
+/** @param {ApiContext} apiContext */
+exports.get = async (apiContext) => {
+	await apiContext.proceed({
+		permissions: ['app.host']
+	});
+	if (apiContext.responsed) return;
+
+	const application = await apiContext.repository.findById('applications', apiContext.params.id);
+	if (application == null) {
+		apiContext.response(404, 'application as premise not found');
+		return;
+	}
+
+	if (!apiContext.applicationsService.existApplicationSecret(application)) {
+		apiContext.response(400, 'application secret has not been generated yet');
+		return;
+	}
+
+	const secret = apiContext.applicationsService.getApplicationSecret(application);
+
+	apiContext.response(200, { secret });
+};
+
+/** @param {ApiContext} apiContext */
+exports.post = async (apiContext) => {
+	await apiContext.proceed({
+		body: {},
+		permissions: ['app.host']
+	});
+	if (apiContext.responsed) return;
+
+	const application = await apiContext.repository.findById('applications', apiContext.params.id);
+	if (application == null) {
+		apiContext.response(404, 'application as premise not found');
+		return;
+	}
+
+	const secret = await apiContext.applicationsService.generateApplicationSecret(application);
+
+	apiContext.response(200, { secret });
+};
