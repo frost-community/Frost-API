eafba5a3b0df10b1b964d44bd69d62935bf44229
marihachi <marihachi0620@gmail.com>
2018-04-15 15:13:13 +0900

    add route GET /auth/valid_credential

---
+++ src/routes/auth/valid_credential.js
@@ -0,0 +1,32 @@
+const ApiContext = require('../../modules/ApiContext');
+const $ = require('cafy').default;
+
+/** @param {ApiContext} apiContext */
+module.exports.get = async (apiContext) => {
+	await apiContext.proceed({
+		query: {
+			screenName: { cafy: $().string() },
+			password: { cafy: $().string() }
+		},
+		scopes: ['auth.host']
+	});
+	if (apiContext.responsed) return;
+
+	const { screenName, password } = apiContext.query;
+
+	if (!apiContext.usersService.validFormatScreenName(screenName)) {
+		apiContext.response(200, { valid: false });
+		return;
+	}
+
+	const user = await apiContext.usersService.findByScreenName(screenName);
+
+	if (user == null) {
+		apiContext.response(200, { valid: false });
+		return;
+	}
+
+	const valid = apiContext.usersService.checkCorrectPassword(user, password);
+
+	apiContext.response(200, { valid });
+};
