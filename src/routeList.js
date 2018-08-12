e30cc8a158fca9a037d459fd6faeb73142671b2f
marihachi <marihachi0620@gmail.com>
2018-08-12 15:02:09 +0900

    update route list

--- src/routeList.js
+++ src/routeList.js
@@ -1,15 +1,10 @@
 module.exports = [
-	'/info/show', // [old] get /
-
-	// == general
-
-	// 全てのユーザーを対象とする、一般公開されたポストを時系列で複数取得
-	'/general/timeline/get', // [old] get /general/timeline
+	'/info/get', // [old] get /
 
 	// == Authorization ==
 
 	// 認証ホスト向けのトークンデータを取得する
-	'/auth/token/lookup', // [old] get /auth/tokens
+	'/auth/token/get', // [old] get /auth/tokens
 
 	// 認証ホスト向けのトークンデータを生成する
 	'/auth/token/create', // [old] post /auth/tokens
@@ -19,25 +14,22 @@ module.exports = [
 
 	// == Applications ==
 
-	// ユーザーが作成したアプリケーション情報を一覧で取得する
-	'/app/list', // [old] get /applications
-
 	// アプリケーションを作成する
 	'/app/create', // [old] post /applications
 
 	// applicationIdを指定してアプリケーション情報を取得する
 	'/app/show', // [old] get /applications/:id
 
+	// ユーザーが作成したアプリケーション情報を一覧で取得する
+	'/app/list', // [old] get /applications
+
 	// applicationIdを指定してapplicationSecretを取得する
 	'/app/secret/show', // [old] get /applications/:id/secret
 
 	// applicationIdを指定してapplicationSecretを生成する
-	'/app/secret/generate', // [old] post /applications/:id/secret
+	'/app/secret/create', // [old] post /applications/:id/secret
 
 	// == Users ==
-
-	// 一つ以上のscreenNameを指定してユーザー情報を取得する (filter=allで全件取得)
-	'/user/list', // [old] get /users
 
 	// アカウントを作成する
 	'/user/create', // [old] post /users
@@ -45,37 +37,34 @@ module.exports = [
 	// ユーザー情報を取得する
 	'/user/show', // [old] get /users/:id
 
+	// 一つ以上のscreenNameを指定してユーザー情報を取得する (filter=allで全件取得)
+	'/user/list', // [old] get /users
+
 	// idを指定してユーザー情報を更新する
 	'/user/update', // [old] patch /users/:id
 
-	// 対象のユーザーが投稿したポストを時系列で複数取得
-	'/user/timeline/get', // [old] get /users/:id/timelines/user
-
-	// 対象のユーザーと、そのユーザーがフォローしてるユーザーが投稿したポストを時系列で複数取得
-	'/home/timeline/get', // [old] get /users/:id/timelines/home
-
-	// フォローの一覧を取得
-	'/user/following/list', // [old] get /users/:id/followings
-
 	// 指定したユーザーが対象のユーザーをフォローしているかどうかを取得
-	'/user/following/show', // [old] get /users/:id/followings/:target_id
+	'/user/relation/get', // [old] get /users/:id/followings/:target_id
 
 	// 指定したユーザーをフォローする
-	'/user/follow', // [old] put /users/:id/followings/:target_id
+	'/user/relation/follow', // [old] put /users/:id/followings/:target_id
 
 	// 指定したユーザーへのフォローを解除する
-	'/user/unfollow', // [old] delete /users/:id/followings/:target_id
+	'/user/relation/unfollow', // [old] delete /users/:id/followings/:target_id
+
+	// フォローの一覧を取得
+	'/user/relation/following/list', // [old] get /users/:id/followings
 
 	// フォロワーの一覧を取得
-	'/user/follower/list', // [old] get /users/:id/followers
+	'/user/relation/follower/list', // [old] get /users/:id/followers
 
 	'/user/storage/status/get', // [old] get /users/:id/storage
-
-	'/user/storage/file/list', // [old] get /users/:id/storage/files
 
 	'/user/storage/file/create', // [old] post /users/:id/storage/files
 
 	'/user/storage/file/show', // [old] get /users/:id/storage/files/:file_id
+
+	'/user/storage/file/list', // [old] get /users/:id/storage/files
 
 	'/user/storage/file/remove', // [old] delete /users/:id/storage/files/:file_id
 
@@ -91,5 +80,16 @@ module.exports = [
 	'/post/create_reference', // [old] post /posts/post_reference
 
 	// idを指定してポストを取得
-	'/post/show' // [old] get /posts/:id
+	'/post/show', // [old] get /posts/:id
+
+	// == Timelines ==
+
+	// 全てのユーザーを対象とする、一般公開されたポストを時系列で複数取得
+	'/timeline/general/get', // [old] get /general/timeline
+
+	// 対象のユーザーが投稿したポストを時系列で複数取得
+	'/timeline/user/get', // [old] get /users/:id/timelines/user
+
+	// 対象のユーザーと、そのユーザーがフォローしてるユーザーが投稿したポストを時系列で複数取得
+	'/timeline/home/get' // [old] get /users/:id/timelines/home
 ];
